b28aab853ed33101f7a0c2131ae29874d23d3d9d
Matheus Barboza <matheus.barboza102@gmail.com>
2022-05-03 21:03:31 -0300

    scrool to top on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import AppRoutes from "./routes";
 
 const App = () => {
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
   return <AppRoutes />;
 };
 
